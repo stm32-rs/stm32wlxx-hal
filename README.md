c9947ef73689fa0561fd4b420eefbd212b05bb56
Alex M <alexmgit@protonmail.com>
2021-04-25 10:02:30 -0700

    Add PKA testsuite.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Embedded rust HAL (hardware abstraction layer) for the STM32WL.
 
-This is a **work in progress**, it is unstable, incomplete, and untested.
+This is a **work in progress**, it is unstable, incomplete, and (mostly) untested.
 
 **Use at your own risk.**
 
@@ -20,3 +20,46 @@ nothing for other people developing on these new chips.
 
 I am also hoping to find other people using these chips with rust who want to
 collaborate on creating a HAL :)
+
+## On-Target Tests
+
+Workspaces with `testsuite` in the name denote an on-target test.
+
+On-target tests use the NUCLEO-WL55JC2, you can find a place purchase this
+[here](https://www.st.com/en/evaluation-tools/nucleo-wl55jc.html#sample-buy).
+
+### On-Target Quickstart
+
+The on-target tests use [defmt-test].
+
+* `cargo install flip-link` ([flip-link])
+* `cargo install probe-run` ([probe-run])
+* `rustup target add --toolchain stable thumbv7m-none-eabi` [rustup]
+* (Linux users only) udev rules are available at [newAM/nucleo-wl55jc2-rs]
+* Connect the nucleo board to your PC via USB.
+* `cargo test -p pka-testsuite --target thumbv7em-none-eabi`
+
+## Unit Tests
+
+Off-target unit tests use the built-in cargo framework.
+The only difference is you must specify the MCU as a feature.
+
+```bash
+cargo test --features stm32wl5x_cm4
+```
+
+Sample output:
+```text
+         INFO  (1/2) running `ecdsa_sign`...
+└─ test::tests::__defmt_test_entry @ tests/test.rs:48
+         INFO  (2/2) running `ecdsa_verify`...
+└─ test::tests::__defmt_test_entry @ tests/test.rs:48
+         INFO  all tests passed!
+└─ test::tests::__defmt_test_entry @ tests/test.rs:48
+```
+
+[newAM/nucleo-wl55jc2-rs]: https://github.com/newAM/nucleo-wl55jc2-rs
+[defmt-test]: https://crates.io/crates/defmt-test
+[flip-link]: https://github.com/knurling-rs/flip-link
+[probe-run]: https://github.com/knurling-rs/probe-run
+[rustup]: https://rustup.rs/
---
+++ pka-testsuite/tests/test.rs
@@ -0,0 +1,86 @@
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _; // global logger
+use panic_probe as _;
+use stm32wl_hal::{
+    pac,
+    pka::{curve::NIST_P256, EcdsaPublicKey, EcdsaSignature, Pka},
+};
+
+// Message hash
+const HASH: [u32; 8] = [
+    0x44acf6b7, 0xe36c1342, 0xc2c58972, 0x04fe0950, 0x4e1e2efb, 0x1a900377, 0xdbc4e7a6, 0xa133ec56,
+];
+
+const PRIVATE_KEY: [u32; 8] = [
+    0x519b423d, 0x715f8b58, 0x1f4fa8ee, 0x59f4771a, 0x5b44c813, 0x0b4e3eac, 0xca54a56d, 0xda72b464,
+];
+
+// Note: in real-world use this should be a one-time random number (nonce).
+// This fixed value is for testing purposes only.
+const INTEGER: [u32; 8] = [
+    0x94a1bbb1, 0x4b906a61, 0xa280f245, 0xf9e93c7f, 0x3b4a6247, 0x824f5d33, 0xb9670787, 0x642a68de,
+];
+
+const SIGNATURE: EcdsaSignature<8> = EcdsaSignature {
+    r_sign: [
+        0xf3ac8061, 0xb514795b, 0x8843e3d6, 0x629527ed, 0x2afd6b1f, 0x6a555a7a, 0xcabb5e6f,
+        0x79c8c2ac,
+    ],
+    s_sign: [
+        0x8bf77819, 0xca05a6b2, 0x786c7626, 0x2bf7371c, 0xef97b218, 0xe96f175a, 0x3ccdda2a,
+        0xcc058903,
+    ],
+};
+
+const PUB_KEY: EcdsaPublicKey<8> = EcdsaPublicKey {
+    curve_pt_x: [
+        0x1ccbe91c, 0x075fc7f4, 0xf033bfa2, 0x48db8fcc, 0xd3565de9, 0x4bbfb12f, 0x3c59ff46,
+        0xc271bf83,
+    ],
+    curve_pt_y: [
+        0xce4014c6, 0x8811f9a2, 0x1a1fdb2c, 0x0e6113e0, 0x6db7ca93, 0xb7404e78, 0xdc7ccd5c,
+        0xa89a4ca9,
+    ],
+};
+
+#[defmt_test::tests]
+mod tests {
+    use super::*;
+
+    #[init]
+    fn init() -> Pka {
+        let dp: pac::Peripherals = pac::Peripherals::take().unwrap();
+        let mut rcc = dp.RCC;
+        rcc.ahb3enr.modify(|_, w| w.pkaen().set_bit());
+        rcc.ahb3enr.read(); // Delay after an RCC peripheral clock enabling
+
+        // TODO: set clocks to 48MHz so the tests execute fater
+
+        Pka::new(dp.PKA, &mut rcc)
+    }
+
+    #[test]
+    fn ecdsa_sign(pka: &mut Pka) {
+        let mut output_signature: EcdsaSignature<8> = EcdsaSignature {
+            r_sign: [0; 8],
+            s_sign: [0; 8],
+        };
+        pka.ecdsa_sign(
+            &NIST_P256,
+            &INTEGER,
+            &PRIVATE_KEY,
+            &HASH,
+            &mut output_signature,
+        )
+        .unwrap();
+        assert_eq!(output_signature, SIGNATURE);
+    }
+
+    #[test]
+    fn ecdsa_verify(pka: &mut Pka) {
+        pka.ecdsa_verify(&NIST_P256, &SIGNATURE, &PUB_KEY, &HASH)
+            .unwrap();
+    }
+}

0a4b16ae8499e17d9d1151ff080d06d567cb26c1
Alex M <alexmgit@protonmail.com>
2021-04-25 12:57:24 -0700

    Add RNG.

---
+++ rng-testsuite/tests/test.rs
@@ -0,0 +1,51 @@
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _; // global logger
+use panic_probe as _;
+use stm32wl_hal::{
+    pac,
+    rng::{rand_core::RngCore, Rng},
+};
+
+#[defmt_test::tests]
+mod tests {
+    use super::*;
+
+    #[init]
+    fn init() -> Rng {
+        let dp: pac::Peripherals = pac::Peripherals::take().unwrap();
+        let mut rcc = dp.RCC;
+        // clock RNG from MSI
+        rcc.ccipr.modify(|_, w| unsafe { w.rngsel().bits(0b11) });
+        rcc.ahb3enr.modify(|_, w| w.rngen().set_bit());
+        rcc.ahb3enr.read(); // Delay after an RCC peripheral clock enabling
+
+        // TODO: set clocks to 48MHz so the tests execute fater
+
+        Rng::new(dp.RNG, &mut rcc)
+    }
+
+    /// This is not a cryptographically secure test, this only ensures that the
+    /// driver is operating nominally, is does not check for hardware
+    /// correctness.
+    #[test]
+    fn random_enough_for_me(rng: &mut Rng) {
+        let mut bytes: [u8; 32] = [0; 32];
+        rng.try_fill_bytes(&mut bytes).unwrap();
+
+        let mut sum: f64 = 0.0;
+        for byte in bytes.iter() {
+            sum += *byte as f64;
+        }
+
+        sum /= bytes.len() as f64;
+
+        const MID: f64 = 127.5;
+        const TOLERANCE: f64 = 15.0;
+        const LO: f64 = MID - TOLERANCE;
+        const HI: f64 = MID + TOLERANCE;
+
+        defmt::assert!(sum > LO && sum < HI, "{:?}", sum);
+    }
+}
---
+++ rng/src/lib.rs
@@ -0,0 +1,224 @@
+//! The RNG is a true random number generator that provides full entropy outputs
+//! to the application as 32-bit samples.
+//! It is composed of a live entropy source (analog) and an internal
+//! conditioning component.
+#![cfg_attr(not(test), no_std)]
+
+use core::{
+    mem::transmute,
+    num::NonZeroU32,
+    sync::atomic::{compiler_fence, Ordering::SeqCst},
+};
+
+pub use rand_core;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "stm32wl5x_cm0p")] {
+        pub use stm32wl::stm32wl5x_cm0p as pac;
+    } else if #[cfg(feature = "stm32wl5x_cm4")] {
+        pub use stm32wl::stm32wl5x_cm4 as pac;
+    } else if #[cfg(feature = "stm32wle5")] {
+        pub use stm32wl::stm32wle5 as pac;
+    } else {
+        core::compile_error!("You must select your hardware with a feature flag");
+    }
+}
+
+/// RNG error types
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum RngError {
+    /// A noise error (seed error) occured and automatic correction failed.
+    UncorrectableNoise,
+    /// RNG frequency is too low.
+    ///
+    /// Check that the RNG is configured correctly.
+    Clock,
+    /// Timeout waiting for the data ready bit.
+    Timeout,
+}
+
+impl From<RngError> for rand_core::Error {
+    fn from(e: RngError) -> Self {
+        match e {
+            RngError::UncorrectableNoise => NonZeroU32::new(1).unwrap().into(),
+            RngError::Clock => NonZeroU32::new(2).unwrap().into(),
+            RngError::Timeout => NonZeroU32::new(3).unwrap().into(),
+        }
+    }
+}
+
+/// RNG driver.
+pub struct Rng {
+    rng: pac::RNG,
+    err_cnt: u32,
+}
+
+impl Rng {
+    /// Create a new `Rng` driver from a RNG peripheral.
+    ///
+    /// This will reset the RNG, but it will not enable clocks for the RNG.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use stm32wl_hal_rng::{pac, Rng};
+    ///
+    /// let dp: pac::Peripherals = pac::Peripherals::take().unwrap();
+    /// let mut rcc = dp.RCC;
+    ///
+    /// // ... setup the system clocks
+    /// // ... take a look at RCC_CCIPR to select the RNG clock source
+    ///
+    /// rcc.ahb3enr.modify(|_, w| w.rngen().set_bit());
+    /// rcc.ahb3enr.read(); // Delay after an RCC peripheral clock enabling
+    ///
+    /// let mut rng = Rng::new(dp.RNG, &mut rcc);
+    /// ```
+    pub fn new(rng: pac::RNG, rcc: &mut pac::RCC) -> Rng {
+        rcc.ahb3rstr.modify(|_, w| w.rngrst().set_bit());
+        rcc.ahb3rstr.modify(|_, w| w.rngrst().clear_bit());
+
+        // RNG configuration A
+        // see table 131 "RNG configurations" in the reference manual
+        #[rustfmt::skip]
+        rng.cr.write(|w| unsafe {
+            w
+                .condrst().set_bit()
+                .nistc().set_bit()
+                .rng_config1().bits(0x0F)
+                .clkdiv().bits(0x0)
+                .rng_config2().bits(0x0)
+                .rng_config3().bits(0xD)
+                .ced().clear_bit()
+                .ie().clear_bit() // interrupt enable
+                .rngen().set_bit()
+        });
+
+        #[rustfmt::skip]
+        rng.cr.write(|w| unsafe {
+            w
+                .condrst().clear_bit()
+                .nistc().set_bit()
+                .rng_config1().bits(0x0F)
+                .clkdiv().bits(0x0)
+                .rng_config2().bits(0x0)
+                .rng_config3().bits(0xD)
+                .ced().clear_bit()
+                .ie().clear_bit() // interrupt enable
+                .rngen().set_bit()
+        });
+
+        // when CONDRST is set to 0 by software its value goes to 0 when the
+        // reset process is done.
+        // It takes about 2 AHB clock cycles + 2 RNG clock cycles
+        while rng.cr.read().condrst().bit_is_set() {
+            compiler_fence(SeqCst);
+        }
+
+        Rng { rng, err_cnt: 0 }
+    }
+
+    /// Returns the number of correctable noise errors that have occured.
+    ///
+    /// This counter will saturate when it hits the maximum value.
+    pub fn noise_error_stat(&self) -> u32 {
+        self.err_cnt
+    }
+
+    /// Reset the correctable noise error counter to zero.
+    pub fn rest_noise_error_stat(&mut self) {
+        self.err_cnt = 0
+    }
+
+    // Reference manual section 22.3.5 "RNG operation"
+    fn read_dr_x4(&mut self) -> [u32; 4] {
+        [
+            self.rng.dr.read().bits(),
+            self.rng.dr.read().bits(),
+            self.rng.dr.read().bits(),
+            self.rng.dr.read().bits(),
+        ]
+    }
+
+    // Reference manual section 22.3.7 "Error management"
+    fn recover_from_noise_error(&mut self) -> Result<(), RngError> {
+        // software reset by writing CONDRST
+        self.rng.cr.modify(|_, w| w.condrst().set_bit());
+        self.rng.cr.modify(|_, w| w.condrst().clear_bit());
+
+        // when CONDRST is set to 0 by software its value goes to 0 when the
+        // reset process is done.
+        // It takes about 2 AHB clock cycles + 2 RNG clock cycles
+        while self.rng.cr.read().condrst().bit_is_set() {
+            compiler_fence(SeqCst);
+        }
+
+        loop {
+            let sr = self.rng.sr.read();
+            if sr.seis().bit_is_set() {
+                return Err(RngError::UncorrectableNoise);
+            }
+            if sr.secs().bit_is_clear() {
+                self.err_cnt = self.err_cnt.saturating_add(1);
+                return Ok(());
+            }
+        }
+    }
+}
+
+impl rand_core::RngCore for Rng {
+    /// Not recommended for use, panics upon errors.
+    fn next_u32(&mut self) -> u32 {
+        let mut bytes: [u8; 4] = [0; 4];
+        self.try_fill_bytes(&mut bytes).unwrap();
+        u32::from_le_bytes(bytes)
+    }
+
+    /// Not recommended for use, panics upon errors.
+    fn next_u64(&mut self) -> u64 {
+        let mut bytes: [u8; 8] = [0; 8];
+        self.try_fill_bytes(&mut bytes).unwrap();
+        u64::from_le_bytes(bytes)
+    }
+
+    /// Not recommended for use, panics upon errors.
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        self.try_fill_bytes(dest).unwrap()
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core::Error> {
+        for chunk in dest.chunks_mut(16) {
+            let mut timeout: u32 = 0;
+            loop {
+                let sr = self.rng.sr.read();
+                if sr.seis().bit_is_set() {
+                    self.recover_from_noise_error()?;
+                } else if sr.ceis().bit_is_set() {
+                    return Err(RngError::Clock.into());
+                } else if sr.drdy().bit_is_set() {
+                    break;
+                }
+
+                // TODO: User selectable timeout duration (or not? this has a fixed duration)
+                // TODO: I dislike everything about this.
+                timeout += 1;
+                if timeout > 100_000 {
+                    return Err(RngError::Timeout.into());
+                }
+            }
+
+            let data: [u32; 4] = self.read_dr_x4();
+            // safety: random data has no requirements for safe transmute.
+            let data: [u8; 16] = unsafe { transmute::<[u32; 4], [u8; 16]>(data) };
+
+            chunk
+                .iter_mut()
+                .zip(data.iter())
+                .for_each(|(buffer_byte, &data_byte)| *buffer_byte = data_byte);
+        }
+
+        Ok(())
+    }
+}
+
+impl rand_core::CryptoRng for Rng {}

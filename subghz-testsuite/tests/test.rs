56db8ead80e2e7ad23724390754000c5754499e3
Alex Martens <alexmgit@protonmail.com>
2021-08-03 21:52:29 -0700

    subghz-testsuite: add LoRa

--- subghz-testsuite/tests/test.rs
+++ subghz-testsuite/tests/test.rs
@@ -17,9 +17,10 @@ use bsp::{
         pac, rcc,
         rng::{self, Rng},
         subghz::{
-            AddrComp, CalibrateImage, CmdStatus, CrcType, FskBandwidth, FskBitrate, FskFdev,
-            FskModParams, FskPulseShape, GenericPacketParams, HeaderType, Irq, Ocp, PaConfig,
-            PaSel, PacketType, PreambleDetection, RampTime, RegMode, RfFreq, StandbyClk, Status,
+            AddrComp, CalibrateImage, CmdStatus, CodingRate, CrcType, FskBandwidth, FskBitrate,
+            FskFdev, FskModParams, FskPulseShape, GenericPacketParams, HeaderType, Irq,
+            LoRaBandwidth, LoRaModParams, LoRaPacketParams, Ocp, PaConfig, PaSel, PacketType,
+            PreambleDetection, RampTime, RegMode, RfFreq, SpreadingFactor, StandbyClk, Status,
             StatusMode, SubGhz, TcxoMode, TcxoTrim, Timeout, TxParams,
         },
     },
@@ -45,7 +46,7 @@ const SYNC_WORD_LEN_BITS: u8 = SYNC_WORD_LEN * 8;
 const TX_BUF_OFFSET: u8 = 128;
 const RX_BUF_OFFSET: u8 = 0;
 
-const PACKET_PARAMS: GenericPacketParams = GenericPacketParams::new()
+const FSK_PACKET_PARAMS: GenericPacketParams = GenericPacketParams::new()
     .set_preamble_len(PREAMBLE_LEN)
     .set_preamble_detection(PreambleDetection::Bit8)
     .set_sync_word_len(SYNC_WORD_LEN_BITS)
@@ -55,11 +56,24 @@ const PACKET_PARAMS: GenericPacketParams = GenericPacketParams::new()
     .set_crc_type(CrcType::Byte2)
     .set_whitening_enable(true);
 
-const MOD_PARAMS: FskModParams = FskModParams::new()
+const LORA_PACKET_PARAMS: LoRaPacketParams = LoRaPacketParams::new()
+    .set_crc_en(true)
+    .set_preamble_len(PREAMBLE_LEN)
+    .set_payload_len(DATA_LEN)
+    .set_invert_iq(false)
+    .set_header_type(HeaderType::Fixed);
+
+const FSK_MOD_PARAMS: FskModParams = FskModParams::new()
     .set_bitrate(FskBitrate::from_bps(50_000))
     .set_pulse_shape(FskPulseShape::None)
     .set_bandwidth(FskBandwidth::Bw58)
     .set_fdev(FskFdev::from_hertz(25_000));
+
+const LORA_MOD_PARAMS: LoRaModParams = LoRaModParams::new()
+    .set_bw(LoRaBandwidth::Bw125)
+    .set_cr(CodingRate::Cr45)
+    .set_ldro_en(true)
+    .set_sf(SpreadingFactor::Sf7);
 
 // configuration for +10 dBm output power
 // see table 35 "PA optimal setting and operating modes"
@@ -104,8 +118,8 @@ async fn aio_wait_irq_inner() {
     sg.aio_set_regulator_mode(RegMode::Ldo).await.unwrap();
     sg.aio_set_sync_word(&SYNC_WORD).await.unwrap();
     sg.aio_set_packet_type(PacketType::Fsk).await.unwrap();
-    sg.aio_set_fsk_mod_params(&MOD_PARAMS).await.unwrap();
-    sg.aio_set_packet_params(&PACKET_PARAMS).await.unwrap();
+    sg.aio_set_fsk_mod_params(&FSK_MOD_PARAMS).await.unwrap();
+    sg.aio_set_packet_params(&FSK_PACKET_PARAMS).await.unwrap();
     sg.aio_calibrate_image(CalibrateImage::ISM_430_440)
         .await
         .unwrap();
@@ -148,13 +162,136 @@ fn tx_or_panic(sg: &mut SubGhz<DmaCh>, rfs: &mut RfSwitch) {
     }
 }
 
+/// This test should be run simultaneously on two boards.
+///
+/// Both radios transmit `b"PING"`.
+///
+/// The first radio to recieve `b"PING"` transmits `b"PONG"` in reply.
+fn ping_pong(sg: &mut SubGhz<DmaCh>, rng: &mut Rng, rfs: &mut RfSwitch, pkt: PacketType) {
+    sg.set_standby(StandbyClk::Rc).unwrap();
+    let status: Status = sg.status().unwrap();
+    assert_eq!(status.mode(), Ok(StatusMode::StandbyRc));
+
+    sg.set_tcxo_mode(&TCXO_MODE).unwrap();
+    sg.set_regulator_mode(RegMode::Ldo).unwrap();
+    sg.set_buffer_base_address(TX_BUF_OFFSET, RX_BUF_OFFSET)
+        .unwrap();
+    sg.set_pa_config(&PA_CONFIG).unwrap();
+    sg.set_pa_ocp(Ocp::Max60m).unwrap();
+    sg.set_tx_params(&TX_PARAMS).unwrap();
+    sg.set_sync_word(&SYNC_WORD).unwrap();
+
+    let status: Status = sg.status().unwrap();
+    assert_eq!(status.mode(), Ok(StatusMode::StandbyRc));
+
+    sg.set_packet_type(pkt).unwrap();
+    match pkt {
+        PacketType::Fsk => {
+            sg.set_fsk_mod_params(&FSK_MOD_PARAMS).unwrap();
+            sg.set_packet_params(&FSK_PACKET_PARAMS).unwrap();
+        }
+        PacketType::LoRa => {
+            sg.set_lora_mod_params(&LORA_MOD_PARAMS).unwrap();
+            sg.set_lora_packet_params(&LORA_PACKET_PARAMS).unwrap();
+        }
+        PacketType::Bpsk => todo!(),
+        PacketType::Msk => todo!(),
+    }
+
+    sg.calibrate_image(CalibrateImage::ISM_430_440).unwrap();
+    sg.set_rf_frequency(&RF_FREQ).unwrap();
+
+    sg.write_buffer(TX_BUF_OFFSET, PING_DATA_BYTES).unwrap();
+
+    const MAX_ATTEMPTS: u32 = 100;
+    let mut attempt: u32 = 0;
+    loop {
+        defmt::info!("Attempt: {}/{}", attempt, MAX_ATTEMPTS);
+
+        // 45 - 300 ms
+        let timeout: Timeout = {
+            let rand_u8: u8 = rng.try_u8().unwrap();
+            let millis: u64 = u64::from(rand_u8).saturating_add(45);
+            let dur: Duration = Duration::from_millis(millis);
+            Timeout::from_duration_sat(dur)
+        };
+
+        defmt::debug!("RX for {:?} ms", timeout.as_duration().as_millis());
+        sg.set_rx(timeout).unwrap();
+
+        let mut rx_loop_cnt: u32 = 0;
+        loop {
+            let (status, irq_status) = sg.irq_status().unwrap();
+
+            if irq_status != 0 {
+                defmt::debug!("IRQ status: 0x{:04X}", irq_status);
+            }
+
+            if irq_status & Irq::Timeout.mask() != 0 {
+                defmt::info!("RX timeout");
+                assert_eq!(status.mode(), Ok(StatusMode::StandbyRc));
+                sg.clear_irq_status(irq_status).unwrap();
+
+                tx_or_panic(sg, rfs);
+                break;
+            } else if irq_status & Irq::RxDone.mask() != 0 {
+                defmt::info!("RX done");
+                assert_eq!(status.mode(), Ok(StatusMode::StandbyRc));
+                sg.clear_irq_status(irq_status).unwrap();
+
+                let (status, len, ptr) = sg.rx_buffer_status().unwrap();
+                assert_eq!(len, DATA_LEN);
+                assert_eq!(status.cmd(), Ok(CmdStatus::Avaliable));
+                assert_eq!(ptr, RX_BUF_OFFSET);
+                assert_eq!(irq_status, 0b10);
+                let mut data_buf: [u8; DATA_LEN as usize] = [0; DATA_LEN as usize];
+                sg.read_buffer(ptr, &mut data_buf).unwrap();
+                match data_buf.as_ref() {
+                    PING_DATA_BYTES => {
+                        defmt::info!("received PING");
+
+                        // respond with pong
+                        sg.write_buffer(TX_BUF_OFFSET, PONG_DATA_BYTES).unwrap();
+
+                        // assume the other radio will get it in 10 transmissions
+                        for _ in 0..10 {
+                            tx_or_panic(sg, rfs);
+                        }
+                    }
+                    PONG_DATA_BYTES => {
+                        defmt::info!("received PONG");
+                    }
+                    _ => panic!("Unknown data: {:?}", data_buf),
+                }
+
+                rfs.set_rx();
+                return;
+            } else {
+                assert_eq!(irq_status, 0);
+                assert_ne!(status.cmd(), Ok(CmdStatus::Avaliable));
+                assert_ne!(status.cmd(), Ok(CmdStatus::ProcessingError));
+                assert_ne!(status.cmd(), Ok(CmdStatus::ExecutionFailure));
+                assert_ne!(status.cmd(), Ok(CmdStatus::Complete));
+
+                rx_loop_cnt = rx_loop_cnt.saturating_add(1);
+                assert!(
+                    rx_loop_cnt < 10000,
+                    "Timeout waiting for RX completion status={}",
+                    status
+                );
+            }
+        }
+
+        attempt = attempt.saturating_add(1);
+        assert!(attempt < MAX_ATTEMPTS);
+    }
+}
+
 #[defmt_test::tests]
 mod tests {
     use super::*;
 
     struct TestArgs {
-        // true if this radio becomes the main
-        is_main: bool,
         sg: SubGhz<DmaCh>,
         rng: Rng,
         rfs: RfSwitch,
@@ -172,8 +309,6 @@ mod tests {
         rfs.set_rx();
 
         let dma = AllDma::split(dp.DMAMUX, dp.DMA1, dp.DMA2, &mut dp.RCC);
-        let tx_dma: DmaCh = dma.d1c1;
-        let rx_dma: DmaCh = dma.d2c1;
 
         #[cfg(feature = "aio")]
         {
@@ -185,15 +320,10 @@ mod tests {
         Rng::set_clock_source(&mut dp.RCC, rng::ClkSrc::MSI);
         let rng: Rng = Rng::new(dp.RNG, &mut dp.RCC);
 
-        let mut sg: SubGhz<DmaCh> = SubGhz::new_with_dma(dp.SPI3, tx_dma, rx_dma, &mut dp.RCC);
+        let mut sg: SubGhz<DmaCh> = SubGhz::new_with_dma(dp.SPI3, dma.d1c1, dma.d2c1, &mut dp.RCC);
         sg.enable_spi_debug(gpioa.pa4, gpioa.pa5, gpioa.pa6, gpioa.pa7);
 
-        TestArgs {
-            is_main: false,
-            sg,
-            rng,
-            rfs,
-        }
+        TestArgs { sg, rng, rfs }
     }
 
     #[test]
@@ -221,124 +351,21 @@ mod tests {
         executor.run();
     }
 
-    /// This test should be run simultaneously on two boards.
-    ///
-    /// Both radios transmit `b"PING"`.
-    ///
-    /// The first radio to recieve `b"PING"` transmits `b"PONG"` in reply.
     #[test]
     fn fsk_ping_pong(ta: &mut TestArgs) {
         let sg: &mut SubGhz<DmaCh> = &mut ta.sg;
         let rng: &mut Rng = &mut ta.rng;
         let rfs: &mut RfSwitch = &mut ta.rfs;
 
-        sg.set_standby(StandbyClk::Rc).unwrap();
-        let status: Status = sg.status().unwrap();
-        assert_eq!(status.mode(), Ok(StatusMode::StandbyRc));
+        ping_pong(sg, rng, rfs, PacketType::Fsk);
+    }
 
-        sg.set_tcxo_mode(&TCXO_MODE).unwrap();
-        sg.set_regulator_mode(RegMode::Ldo).unwrap();
-        sg.set_buffer_base_address(TX_BUF_OFFSET, RX_BUF_OFFSET)
-            .unwrap();
-        sg.set_pa_config(&PA_CONFIG).unwrap();
-        sg.set_pa_ocp(Ocp::Max60m).unwrap();
-        sg.set_tx_params(&TX_PARAMS).unwrap();
-        sg.set_sync_word(&SYNC_WORD).unwrap();
+    #[test]
+    fn lora_ping_pong(ta: &mut TestArgs) {
+        let sg: &mut SubGhz<DmaCh> = &mut ta.sg;
+        let rng: &mut Rng = &mut ta.rng;
+        let rfs: &mut RfSwitch = &mut ta.rfs;
 
-        let status: Status = sg.status().unwrap();
-        assert_eq!(status.mode(), Ok(StatusMode::StandbyRc));
-
-        sg.set_packet_type(PacketType::Fsk).unwrap();
-        sg.set_fsk_mod_params(&MOD_PARAMS).unwrap();
-        sg.set_packet_params(&PACKET_PARAMS).unwrap();
-        sg.calibrate_image(CalibrateImage::ISM_430_440).unwrap();
-        sg.set_rf_frequency(&RF_FREQ).unwrap();
-
-        sg.write_buffer(TX_BUF_OFFSET, PING_DATA_BYTES).unwrap();
-
-        const MAX_ATTEMPTS: u32 = 100;
-        let mut attempt: u32 = 0;
-        loop {
-            defmt::info!("Attempt: {}/{}", attempt, MAX_ATTEMPTS);
-
-            // 45 - 300 ms
-            let timeout: Timeout = {
-                let rand_u8: u8 = rng.try_u8().unwrap();
-                let millis: u64 = u64::from(rand_u8).saturating_add(45);
-                let dur: Duration = Duration::from_millis(millis);
-                Timeout::from_duration_sat(dur)
-            };
-
-            defmt::debug!("RX for {:?} ms", timeout.as_duration().as_millis());
-            sg.set_rx(timeout).unwrap();
-
-            let mut rx_loop_cnt: u32 = 0;
-            loop {
-                let (status, irq_status) = sg.irq_status().unwrap();
-
-                if irq_status != 0 {
-                    defmt::debug!("IRQ status: 0x{:04X}", irq_status);
-                }
-
-                if irq_status & Irq::Timeout.mask() != 0 {
-                    defmt::info!("RX timeout");
-                    assert_eq!(status.mode(), Ok(StatusMode::StandbyRc));
-                    sg.clear_irq_status(irq_status).unwrap();
-
-                    tx_or_panic(sg, rfs);
-                    break;
-                } else if irq_status & Irq::RxDone.mask() != 0 {
-                    defmt::info!("RX done");
-                    assert_eq!(status.mode(), Ok(StatusMode::StandbyRc));
-                    sg.clear_irq_status(irq_status).unwrap();
-
-                    let (status, len, ptr) = sg.rx_buffer_status().unwrap();
-                    assert_eq!(len, DATA_LEN);
-                    assert_eq!(status.cmd(), Ok(CmdStatus::Avaliable));
-                    assert_eq!(ptr, RX_BUF_OFFSET);
-                    assert_eq!(irq_status, 0b10);
-                    let mut data_buf: [u8; DATA_LEN as usize] = [0; DATA_LEN as usize];
-                    sg.read_buffer(ptr, &mut data_buf).unwrap();
-                    match data_buf.as_ref() {
-                        PING_DATA_BYTES => {
-                            defmt::info!("received PING");
-                            ta.is_main = false;
-
-                            // respond with pong
-                            sg.write_buffer(TX_BUF_OFFSET, PONG_DATA_BYTES).unwrap();
-
-                            // assume the other radio will get it in 10 transmissions
-                            for _ in 0..10 {
-                                tx_or_panic(sg, rfs);
-                            }
-                        }
-                        PONG_DATA_BYTES => {
-                            defmt::info!("received PONG");
-                            ta.is_main = true
-                        }
-                        _ => panic!("Unknown data: {:?}", data_buf),
-                    }
-
-                    rfs.set_rx();
-                    return;
-                } else {
-                    assert_eq!(irq_status, 0);
-                    assert_ne!(status.cmd(), Ok(CmdStatus::Avaliable));
-                    assert_ne!(status.cmd(), Ok(CmdStatus::ProcessingError));
-                    assert_ne!(status.cmd(), Ok(CmdStatus::ExecutionFailure));
-                    assert_ne!(status.cmd(), Ok(CmdStatus::Complete));
-
-                    rx_loop_cnt = rx_loop_cnt.saturating_add(1);
-                    assert!(
-                        rx_loop_cnt < 10000,
-                        "Timeout waiting for RX completion status={}",
-                        status
-                    );
-                }
-            }
-
-            attempt = attempt.saturating_add(1);
-            assert!(attempt < MAX_ATTEMPTS);
-        }
+        ping_pong(sg, rng, rfs, PacketType::LoRa);
     }
 }

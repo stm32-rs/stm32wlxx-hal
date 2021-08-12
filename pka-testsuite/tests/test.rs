ae727812921f54618fe3333d6a95b2fc155429f6
Alex Martens <alexmgit@protonmail.com>
2021-08-12 10:27:06 -0700

    pka: add non-blocking methods

--- pka-testsuite/tests/test.rs
+++ pka-testsuite/tests/test.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![no_main]
 
-use core::ptr::write_volatile;
 use defmt::unwrap;
 use defmt_rtt as _; // global logger
 use panic_probe as _;
@@ -9,13 +8,14 @@ use stm32wl_hal::{
     pac::{self, DWT},
     pka::{curve::NIST_P256, EcdsaPublicKey, EcdsaSignature, Pka},
     rcc,
+    util::reset_cycle_count,
 };
 
 const FREQ: u32 = 48_000_000;
-const CYC_PER_US: u32 = FREQ / 1000 / 1000;
+const CYC_PER_MICRO: u32 = FREQ / 1000 / 1000;
 
 // WARNING will wrap-around eventually, use this for relative timing only
-defmt::timestamp!("{=u32:µs}", DWT::get_cycle_count() / CYC_PER_US);
+defmt::timestamp!("{=u32:µs}", DWT::get_cycle_count() / CYC_PER_MICRO);
 
 // Message hash
 const HASH: [u32; 8] = [
@@ -32,30 +32,35 @@ const INTEGER: [u32; 8] = [
     0x94a1bbb1, 0x4b906a61, 0xa280f245, 0xf9e93c7f, 0x3b4a6247, 0x824f5d33, 0xb9670787, 0x642a68de,
 ];
 
+const R_SIGN: [u32; 8] = [
+    0xf3ac8061, 0xb514795b, 0x8843e3d6, 0x629527ed, 0x2afd6b1f, 0x6a555a7a, 0xcabb5e6f, 0x79c8c2ac,
+];
+const S_SIGN: [u32; 8] = [
+    0x8bf77819, 0xca05a6b2, 0x786c7626, 0x2bf7371c, 0xef97b218, 0xe96f175a, 0x3ccdda2a, 0xcc058903,
+];
+
+const CURVE_PT_X: [u32; 8] = [
+    0x1ccbe91c, 0x075fc7f4, 0xf033bfa2, 0x48db8fcc, 0xd3565de9, 0x4bbfb12f, 0x3c59ff46, 0xc271bf83,
+];
+
+const CURVE_PT_Y: [u32; 8] = [
+    0xce4014c6, 0x8811f9a2, 0x1a1fdb2c, 0x0e6113e0, 0x6db7ca93, 0xb7404e78, 0xdc7ccd5c, 0xa89a4ca9,
+];
+
 const SIGNATURE: EcdsaSignature<8> = EcdsaSignature {
-    r_sign: [
-        0xf3ac8061, 0xb514795b, 0x8843e3d6, 0x629527ed, 0x2afd6b1f, 0x6a555a7a, 0xcabb5e6f,
-        0x79c8c2ac,
-    ],
-    s_sign: [
-        0x8bf77819, 0xca05a6b2, 0x786c7626, 0x2bf7371c, 0xef97b218, 0xe96f175a, 0x3ccdda2a,
-        0xcc058903,
-    ],
+    r_sign: &R_SIGN,
+    s_sign: &S_SIGN,
 };
 
 const PUB_KEY: EcdsaPublicKey<8> = EcdsaPublicKey {
-    curve_pt_x: [
-        0x1ccbe91c, 0x075fc7f4, 0xf033bfa2, 0x48db8fcc, 0xd3565de9, 0x4bbfb12f, 0x3c59ff46,
-        0xc271bf83,
-    ],
-    curve_pt_y: [
-        0xce4014c6, 0x8811f9a2, 0x1a1fdb2c, 0x0e6113e0, 0x6db7ca93, 0xb7404e78, 0xdc7ccd5c,
-        0xa89a4ca9,
-    ],
+    curve_pt_x: &CURVE_PT_X,
+    curve_pt_y: &CURVE_PT_Y,
 };
 
 #[defmt_test::tests]
 mod tests {
+    use stm32wl_hal::pka::{EcdsaSignError, EcdsaSignature, EcdsaVerifyError};
+
     use super::*;
 
     #[init]
@@ -67,31 +72,118 @@ mod tests {
 
         cp.DCB.enable_trace();
         cp.DWT.enable_cycle_counter();
-        // reset the cycle counter
-        const DWT_CYCCNT: usize = 0xE0001004;
-        unsafe { write_volatile(DWT_CYCCNT as *mut u32, 0) };
+        reset_cycle_count(&mut cp.DWT);
 
         Pka::new(dp.PKA, &mut dp.RCC)
     }
 
     #[test]
     fn ecdsa_sign(pka: &mut Pka) {
-        let mut output_signature: EcdsaSignature<8> = EcdsaSignature {
-            r_sign: [0; 8],
-            s_sign: [0; 8],
-        };
+        let mut r_sign: [u32; 8] = [0; 8];
+        let mut s_sign: [u32; 8] = [0; 8];
         unwrap!(pka.ecdsa_sign(
             &NIST_P256,
             &INTEGER,
             &PRIVATE_KEY,
             &HASH,
-            &mut output_signature,
+            &mut r_sign,
+            &mut s_sign,
         ));
-        defmt::assert_eq!(output_signature, SIGNATURE);
+        defmt::assert_eq!(r_sign, R_SIGN);
+        defmt::assert_eq!(s_sign, S_SIGN);
+    }
+
+    #[test]
+    fn ecdsa_sign_nb(pka: &mut Pka) {
+        unwrap!(pka.ecdsa_sign_start(&NIST_P256, &INTEGER, &PRIVATE_KEY, &HASH));
+        let mut r_sign: [u32; 8] = [0; 8];
+        let mut s_sign: [u32; 8] = [0; 8];
+        unwrap!(nb::block!(pka.ecdsa_sign_result(&mut r_sign, &mut s_sign)));
+        defmt::assert_eq!(r_sign, R_SIGN);
+        defmt::assert_eq!(s_sign, S_SIGN);
+    }
+
+    #[test]
+    fn ecdsa_sign_ram_error(pka: &mut Pka) {
+        unwrap!(pka.ecdsa_verify_start(&NIST_P256, &SIGNATURE, &PUB_KEY, &HASH));
+        let mut r_sign: [u32; 8] = [0; 8];
+        let mut s_sign: [u32; 8] = [0; 8];
+        defmt::assert_eq!(
+            pka.ecdsa_sign(
+                &NIST_P256,
+                &INTEGER,
+                &PRIVATE_KEY,
+                &HASH,
+                &mut r_sign,
+                &mut s_sign,
+            ),
+            Err(EcdsaSignError::Ram)
+        );
+        unwrap!(nb::block!(pka.ecdsa_verify_result()));
+    }
+
+    #[test]
+    fn ecdsa_sign_mode_error(pka: &mut Pka) {
+        unwrap!(pka.ecdsa_verify_start(&NIST_P256, &SIGNATURE, &PUB_KEY, &HASH));
+        let mut r_sign: [u32; 8] = [0; 8];
+        let mut s_sign: [u32; 8] = [0; 8];
+        defmt::assert_eq!(
+            nb::block!(pka.ecdsa_sign_result(&mut r_sign, &mut s_sign)),
+            Err(EcdsaSignError::Mode { mode: 0b100110 })
+        );
+        unwrap!(nb::block!(pka.ecdsa_verify_result()));
     }
 
     #[test]
     fn ecdsa_verify(pka: &mut Pka) {
         unwrap!(pka.ecdsa_verify(&NIST_P256, &SIGNATURE, &PUB_KEY, &HASH))
+    }
+
+    #[test]
+    fn ecdsa_verify_nb(pka: &mut Pka) {
+        unwrap!(pka.ecdsa_verify_start(&NIST_P256, &SIGNATURE, &PUB_KEY, &HASH));
+        unwrap!(nb::block!(pka.ecdsa_verify_result()));
+    }
+
+    #[test]
+    fn ecdsa_verify_ram_err(pka: &mut Pka) {
+        unwrap!(pka.ecdsa_sign_start(&NIST_P256, &INTEGER, &PRIVATE_KEY, &HASH));
+        defmt::assert_eq!(
+            pka.ecdsa_verify(&NIST_P256, &SIGNATURE, &PUB_KEY, &HASH),
+            Err(EcdsaVerifyError::Ram)
+        );
+        let mut r_sign: [u32; 8] = [0; 8];
+        let mut s_sign: [u32; 8] = [0; 8];
+        unwrap!(nb::block!(pka.ecdsa_sign_result(&mut r_sign, &mut s_sign)));
+        defmt::assert_eq!(r_sign, R_SIGN);
+        defmt::assert_eq!(s_sign, S_SIGN);
+    }
+
+    #[test]
+    fn ecdsa_verify_mode_err(pka: &mut Pka) {
+        unwrap!(pka.ecdsa_sign_start(&NIST_P256, &INTEGER, &PRIVATE_KEY, &HASH));
+        defmt::assert_eq!(
+            nb::block!(pka.ecdsa_verify_result()),
+            Err(EcdsaVerifyError::Mode { mode: 0b100100 })
+        );
+        let mut r_sign: [u32; 8] = [0; 8];
+        let mut s_sign: [u32; 8] = [0; 8];
+        unwrap!(nb::block!(pka.ecdsa_sign_result(&mut r_sign, &mut s_sign)));
+        defmt::assert_eq!(r_sign, R_SIGN);
+        defmt::assert_eq!(s_sign, S_SIGN);
+    }
+
+    #[test]
+    fn ecdsa_verify_invalid_err(pka: &mut Pka) {
+        let mut s_sign: [u32; 8] = S_SIGN;
+        s_sign[0] ^= 0b1;
+        let signature: EcdsaSignature<8> = EcdsaSignature {
+            s_sign: &s_sign,
+            r_sign: &R_SIGN,
+        };
+        defmt::assert_eq!(
+            pka.ecdsa_verify(&NIST_P256, &signature, &PUB_KEY, &HASH),
+            Err(EcdsaVerifyError::Invalid)
+        )
     }
 }

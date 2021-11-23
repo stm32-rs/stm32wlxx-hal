bc6bae3c91e03ddd163406de815be090d7b23251
Rick van der Wal <rickvanderwalvis@gmail.com>
2021-11-23 11:06:31 +0100

    Implement transmit and receive traits

--- hal/src/subghz/sx126x.rs
+++ hal/src/subghz/sx126x.rs
@@ -2,69 +2,91 @@ use embedded_hal::blocking::delay::DelayUs;
 use lorawan_device::radio::{LoRaChannel, LoRaInfo, LoRaState};
 use radio::{Busy, Channel, Receive, State, Transmit};
 
-use crate::subghz::{rfbusyms, SubGhz};
+use crate::spi::Spi3;
+use crate::subghz;
+use crate::subghz::{Irq, SubGhz, Timeout};
 use crate::subghz::rfs::{RfSwRx, RfSwTx};
 
 /// Sx126x radio.
 pub struct Sx126x<MISO, MOSI, RFS> {
-    subghz: SubGhz<MISO, MOSI>,
+    sg: SubGhz<MISO, MOSI>,
     rfs: RFS,
 }
 
 impl<MISO, MOSI, RFS> Sx126x<MISO, MOSI, RFS>
-    where RFS: RfSwRx + RfSwTx
+    where Spi3<MISO, MOSI>: embedded_hal::blocking::spi::Transfer<u8, Error=subghz::Error>,
+          Spi3<MISO, MOSI>: embedded_hal::blocking::spi::Write<u8, Error=subghz::Error>,
+          RFS: RfSwRx + RfSwTx
 {
     /// Creates a new Sx126x radio.
-    pub fn new(subghz: SubGhz<MISO, MOSI>, rfs: RFS) -> Self {
+    pub fn new(sg: SubGhz<MISO, MOSI>, rfs: RFS) -> Self {
+        // TODO: Setup
         Sx126x {
-            subghz,
+            sg,
             rfs,
         }
     }
 
     /// Returns the internal Sub-GHz radio peripheral.
     pub fn as_subghz(&self) -> &SubGhz<MISO, MOSI> {
-        &self.subghz
+        &self.sg
     }
 
     /// Returns a mutable reference to the internal Sub-GHz radio peripheral.
     pub fn as_mut_subghz(&mut self) -> &mut SubGhz<MISO, MOSI> {
-        &mut self.subghz
+        &mut self.sg
     }
 }
 
 impl<MISO, MOSI, RFS> Transmit for Sx126x<MISO, MOSI, RFS>
-    where RFS: RfSwRx + RfSwTx
+    where Spi3<MISO, MOSI>: embedded_hal::blocking::spi::Transfer<u8, Error=subghz::Error>,
+          Spi3<MISO, MOSI>: embedded_hal::blocking::spi::Write<u8, Error=subghz::Error>,
+          RFS: RfSwRx + RfSwTx
 {
     type Error = Error;
 
-    fn start_transmit(&mut self, _data: &[u8]) -> Result<(), Self::Error> {
+    fn start_transmit(&mut self, data: &[u8]) -> Result<(), Self::Error> {
+        self.sg.write_buffer(0, data)?;
         self.rfs.set_tx();
-        todo!()
+        self.sg.set_tx(Timeout::from_millis_sat(100))?;
+        Ok(())
     }
 
     fn check_transmit(&mut self) -> Result<bool, Self::Error> {
-        todo!()
+        let (_, irq_status) = self.sg.irq_status()?;
+        self.sg.clear_irq_status(irq_status)?;
+        Ok(irq_status & Irq::TxDone.mask() != 0)
     }
 }
 
 impl<MISO, MOSI, RFS> Receive for Sx126x<MISO, MOSI, RFS>
-    where RFS: RfSwRx + RfSwTx
+    where Spi3<MISO, MOSI>: embedded_hal::blocking::spi::Transfer<u8, Error=subghz::Error>,
+          Spi3<MISO, MOSI>: embedded_hal::blocking::spi::Write<u8, Error=subghz::Error>,
+          RFS: RfSwRx + RfSwTx
 {
     type Error = Error;
     type Info = LoRaInfo;
 
     fn start_receive(&mut self) -> Result<(), Self::Error> {
         self.rfs.set_rx();
-        todo!()
+        self.sg.set_rx(Timeout::from_millis_sat(100))?;
+        Ok(())
     }
 
-    fn check_receive(&mut self, _restart: bool) -> Result<bool, Self::Error> {
-        todo!()
+    fn check_receive(&mut self, _: bool) -> Result<bool, Self::Error> {
+        let (_, irq_status) = self.sg.irq_status()?;
+        self.sg.clear_irq_status(irq_status)?;
+        Ok(irq_status & Irq::RxDone.mask() != 0)
     }
 
-    fn get_received(&mut self, _buff: &mut [u8]) -> Result<(usize, Self::Info), Self::Error> {
-        todo!()
+    fn get_received(&mut self, buf: &mut [u8]) -> Result<(usize, Self::Info), Self::Error> {
+        let (_, len, ptr) = self.sg.rx_buffer_status()?;
+        let size = usize::from(len);
+        let data: &mut [u8] = &mut buf[..size];
+        self.sg.read_buffer(ptr, data)?;
+        // TODO: get info
+        let info = LoRaInfo::default();
+        Ok((size, info))
     }
 }
 
@@ -94,7 +116,7 @@ impl<MISO, MOSI, RFS> Busy for Sx126x<MISO, MOSI, RFS> {
     type Error = Error;
 
     fn is_busy(&mut self) -> Result<bool, Self::Error> {
-        Ok(rfbusyms())
+        Ok(subghz::rfbusyms())
     }
 }
 
@@ -105,4 +127,12 @@ impl<MISO, MOSI, RFS> DelayUs<u32> for Sx126x<MISO, MOSI, RFS> {
 }
 
 #[derive(Debug)]
-pub enum Error {}
+pub enum Error {
+    SubGhz(subghz::Error),
+}
+
+impl From<subghz::Error> for Error {
+    fn from(err: subghz::Error) -> Self {
+        Error::SubGhz(err)
+    }
+}

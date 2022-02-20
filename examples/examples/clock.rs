c442b6576c6af24f7f6a3004b3c6b688b27e0498
Alex Martens <alex@thinglab.org>
2022-02-20 13:43:12 -0800

    examples/{clock,info}: defmt::info -> defmt::println

--- examples/examples/clock.rs
+++ examples/examples/clock.rs
@@ -11,11 +11,11 @@ use stm32wlxx_hal::{self as hal, pac, rcc};
 fn main() -> ! {
     let dp: pac::Peripherals = defmt::unwrap!(pac::Peripherals::take());
 
-    defmt::info!("sysclk_hz: {}", rcc::sysclk_hz(&dp.RCC));
-    defmt::info!("hclk1_hz: {}", rcc::hclk1_hz(&dp.RCC));
-    defmt::info!("hclk2_hz: {}", rcc::hclk2_hz(&dp.RCC));
-    defmt::info!("hclk3_hz: {}", rcc::hclk3_hz(&dp.RCC));
-    defmt::info!("lsi_hz: {}", rcc::lsi_hz(&dp.RCC));
+    defmt::println!("sysclk_hz: {}", rcc::sysclk_hz(&dp.RCC));
+    defmt::println!("hclk1_hz: {}", rcc::hclk1_hz(&dp.RCC));
+    defmt::println!("hclk2_hz: {}", rcc::hclk2_hz(&dp.RCC));
+    defmt::println!("hclk3_hz: {}", rcc::hclk3_hz(&dp.RCC));
+    defmt::println!("lsi_hz: {}", rcc::lsi_hz(&dp.RCC));
 
     loop {
         hal::cortex_m::asm::bkpt();
